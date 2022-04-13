4dcc3d7ffb13cd0492ad192105a485c8d5aa710b
shuyshuys <ilvrynsetsuna@protonmail.com>
2022-04-14 05:11:30 +0700

    add Postgres Reader for makanan and minuman

--- src/main/java/com/shuyshuys/appwarung/PostReader.java
+++ src/main/java/com/shuyshuys/appwarung/PostReader.java
@@ -1,5 +1,62 @@
 package com.shuyshuys.appwarung;
 
-public class PostReader {
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class PostReader {
+    static int idx_count;
+    static int idx_max_makanan;
+    static int idx_max_minuman;
+    static int idx_max_pembeli;
+
+    // private static String line;
+
+    public static void getPostReader(String category) {
+        try {
+            add.border();
+
+            if (category == "makanan") {
+
+                String count = "SELECT category_barang,COUNT(*) FROM barang WHERE category_barang = 'makanan' GROUP BY category_barang;";
+                ResultSet rs = Post.getCon().createStatement().executeQuery(count);
+                while (rs.next()) {
+                    cMenuCLI.makanan = new cMakanan[rs.getInt(2) + 10];
+                }
+
+                String sql = "SELECT * FROM barang WHERE category_barang = '" + category + "';";
+                Statement statement = Post.getCon().createStatement();
+                ResultSet res = statement.executeQuery(sql);
+                while (res.next()) {
+                    cMenuCLI.makanan[add.idxMakanan] = new cMakanan(
+                            Integer.parseInt(res.getString(1)),
+                            res.getString(2),
+                            Integer.parseInt(res.getString(3)));
+                    cMenuCLI.makanan[add.idxMakanan].ToString();
+                    add.idxMakanan++;
+                }
+            } else if (category == "minuman") {
+
+                String count = "SELECT category_barang,COUNT('minuman') FROM barang WHERE category_barang = 'minuman' GROUP BY category_barang;";
+                ResultSet rs = Post.getCon().createStatement().executeQuery(count);
+                while (rs.next()) {
+                    cMenuCLI.minuman = new cMinuman[rs.getInt(2) + 10];
+                }
+
+                String sql = "SELECT * FROM barang WHERE category_barang = '" + category + "';";
+                Statement stmt = Post.getCon().createStatement();
+                ResultSet res = stmt.executeQuery(sql);
+                while (res.next()) {
+                    cMenuCLI.minuman[add.idxMinuman] = new cMinuman(
+                            res.getInt(1),
+                            res.getString(2),
+                            res.getInt(3));
+                    cMenuCLI.minuman[add.idxMinuman].ToString();
+                    add.idxMinuman++;
+                }
+            }
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
