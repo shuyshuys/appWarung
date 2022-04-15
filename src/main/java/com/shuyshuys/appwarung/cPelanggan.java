8628035b255be168228aaa96133408a937c2eb5e
shuyshuys <ilvrynsetsuna@protonmail.com>
2022-04-15 10:33:05 +0700

    add delete member

--- src/main/java/com/shuyshuys/appwarung/cPelanggan.java
+++ src/main/java/com/shuyshuys/appwarung/cPelanggan.java
@@ -7,7 +7,8 @@ public class cPelanggan {
         add.border();
         System.out.println("1. Buat Pelanggan Baru");
         System.out.println("2. Lihat Pelanggan");
-        System.out.println("3. Keluar");
+        System.out.println("3. Hapus Pelanggan");
+        System.out.println("4. Keluar");
         System.out.print("Pilihan: ");
         int pilih = add.sc().nextInt();
         switch (pilih) {
@@ -38,6 +39,44 @@ public class cPelanggan {
                 Pelanggan();
                 break;
             case 3:
+                add.cls();
+                add.border();
+                System.out.println(" ⁘⁘ Menu Hapus Pelanggan");
+                add.border();
+                add.listPembeli();
+                add.border();
+                System.out.print("Masukkan Nama : ");
+                String hapusNama = add.sc().nextLine();
+                boolean found2 = false;
+                for (int i = 0; i < add.idxPembeli; i++) {
+                    if (cTransaksi.pembeli[i].getNama().equalsIgnoreCase(hapusNama)) {
+                        found2 = true;
+                        System.out.println(cTransaksi.pembeli[i].ToString());
+                        System.out.print("Apakah anda yakin ingin menghapus? (y/n)  : ");
+                        String jawab = add.sc().next();
+                        if (jawab.equals("y")) {
+                            // cTransaksi.pembeli[i] = cTransaksi.pembeli[add.idxPembeli - 1];
+                            for (int j = i; j < add.idxPembeli; j++) {
+                                if (j == add.idxMinuman - 1) {
+                                    cTransaksi.pembeli[j] = null;
+                                } else {
+                                    cTransaksi.pembeli[j] = cTransaksi.pembeli[j + 1];
+                                }
+                            }
+                            add.idxPembeli--;
+                            System.out.println("  ⁂ Member berhasil dihapus");
+                        } else {
+                            System.out.println("  ⁂ Member tidak dihapus");
+                            break;
+                        }
+                        if (!found2) {
+                            System.out.println("  ⁂ Member tidak ditemukan");
+                        }
+                    }
+                }
+                add.sleep();
+                break;
+            case 4:
                 cMenuCLI.menuCLI();
                 break;
             default:
